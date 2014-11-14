3a590c0e04d4e96ecf3f9525b3c6928531c9fb84
MRobinson95 <ROBM2_13@uni.worc.ac.uk>
2014-11-14 10:51:25 +0000

    ddd

--- TrueFalse/www/js/index.js
+++ TrueFalse/www/js/index.js
@@ -1,11 +1,17 @@
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
-$(document).on("pagecreate","#pageone",function(){
+$(document).on("pagecreate","#pageone",function()
+{
   
-  	$('#randomise').on("tap",function(){
-    	$(this).text = "TEST"; 
+  	$('#randomise').on("tap",function()
+	{
+    	alert(random()); 
  	});                       
 
+	function random() 
+	{ 
+		return !Math.round(Math.random()); 
+	}
 	
 
 });
