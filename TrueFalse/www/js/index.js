b1a6cf0392a065a1f6cb04b0bbe63731c3ac4573
MRobinson95 <ROBM2_13@uni.worc.ac.uk>
2014-11-14 10:44:43 +0000

    adasd

---
+++ TrueFalse/www/js/index.js
@@ -0,0 +1,11 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
+$(document).on("pagecreate","#pageone",function(){
+  
+  	$('#randomise').on("tap",function(){
+    	$(this).value = !Math.round(Math.random()); 
+ 	});                       
+
+	
+
+});
